cf1927d08c3fa478f2d2af945e3e28c1385d1aa6
Rafael Piedra <rafaelpiedra_2002@hotmail.es>
2022-03-24 14:37:18 -0500

    se implementó la función agregar

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-window.addEventListener("DOMContentLoaded", () => {
   const title = document.getElementById("title");
   const descripcion = document.getElementById("descripcion");
-  const btn_agregar = document.getElementById("btn-agregar");
-  btn_agregar.addEventListener("click",()=>{
-      
-  })
-});
+  const tbody = document.querySelector("#tbody");
+  let contador = 1;
+
+function agregar() {
+  let content = `
+  <th scope="row">${contador++}</th>
+  <td>${title.value}</td>
+  <td>${descripcion.value}</td>
+  <td>
+  <div class="container">
+  <button type="button" class="btn btn-success mb-2 my-4" id="btn-agregar" onclick="">Editar</button>
+  <button type="button" class="btn btn-danger mb-2 my-4" id="btn-agregar" onclick="eliminar()">Eliminar</button>
+  </div>
+  </td>
+  `;
+  const child = document.createElement("tr");
+  child.innerHTML = content;
+  tbody.append(child);
+}
+
+
+function eliminar() {
+  tbody.remove(tbody.lastChild)
+}
